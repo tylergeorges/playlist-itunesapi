34b042460526269ebcb2982ef21196fbf08a7865
tylergeorges <tmg320v@gmail.com>
2022-03-08 13:10:46 -0600

    latestbuildapi

--- src/components/dropbutton.js
+++ src/components/dropbutton.js
@@ -12,7 +12,7 @@ const Dropbutton = (props) =>{
         e.preventDefault()
         setOpen((prevState => !prevState))
                                          
-        axios.get(`https://itunes.apple.com/lookup?id=${props.value}`)
+        axios.get(`/api/lookup?id=${props.value}`)
             .then(data => {
                data.data.results.map(track =>{
                    props.addSong(track)
